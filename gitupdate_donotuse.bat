55c8ae1a82b1f6ecf9ae4f6fd8e40716271ed6f2
fanyujie <1789239032@qq.com>
2021-11-21 16:28:03 +0800

    1625

--- gitupdate_donotuse.bat
+++ gitupdate_donotuse.bat
@@ -2,7 +2,7 @@
 
 
 git add ./  
-git commit -m %date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%
+git commit -m "1625"
 git branch -M main_flask
-git push -u origin main_flask
+git push -u  -f  origin main_flask
 pause
